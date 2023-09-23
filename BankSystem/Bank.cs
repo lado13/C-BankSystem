fbda6060cf92f50942d9b240283c8fb12a7d1590
lado13 <119516940+lado13@users.noreply.github.com>
2023-09-24 00:30:35 +0400

    Add project files.

---
+++ BankSystem/Bank.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSystem
+{
+    internal class Bank
+    {
+        public string AccountNumber { get; set; }
+        public decimal Balance { get; set; }
+        public User User { get; set; }
+
+
+
+
+
+        public static string Transaction(Bank user1, Bank user2, decimal amount)
+        {
+            if (user1.Balance >= amount)
+            {
+                user1.Balance -= amount;
+                user2.Balance += amount;     
+            }
+            
+            Console.WriteLine($"{user1.User.UserName} transfer to {user2.User.UserName}  {amount} $  trasaction time {DateTime.Now}");
+            return $"{user1.User.UserName} transfer to {user2.User.UserName}  {amount} $  trasaction time {DateTime.Now}";
+        }
+
+        public static string AddCash(decimal amount, Bank user)
+        {
+            if (amount > 0)
+            {
+                user.Balance += amount;
+                Console.WriteLine($"Added {amount:C} to the account. new balance {user.Balance:C}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+
+            return $"Added {amount:C} to the account. new balance {user.Balance:C} Time {DateTime.Now}";
+        }
+
+        public void CashOut(decimal amount)
+        {
+            if (amount > 0 && amount <= Balance)
+            {
+                Balance -= amount;
+                Console.WriteLine($"Cashed out {amount:C} from the account. New balance: {Balance:C}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid amount");
+            }
+        }
+
+
+
+        public static void Writer(FileInfo f, string content)
+        {
+            using(StreamWriter sw = f.AppendText())
+            {
+                sw.WriteLine(content);
+            }
+        }
+        public static void Writer(string path, string content)
+        {
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine(content);
+            }
+        }
+
+
+        public static void Reader(string path)
+        {
+            using (StreamReader sr = File.OpenText(path))
+            {
+                Console.WriteLine(sr.ReadToEnd());
+            } 
+        }
+
+
+
+
+
+        public override string ToString()
+        {
+            return $"AccountNumber {AccountNumber} Balance {Balance} User {User}";
+        }
+    }
+}
---
+++ BankSystem/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Bank user1 = new Bank()
+            {
+                AccountNumber = "G45",
+                Balance = 500,
+                User = new User()
+                {
+                    UserName = "Jony",
+                    LastName = "Smith",
+                    Age = 24,
+
+                }
+            };
+
+
+            Bank user2 = new Bank()
+            {
+                AccountNumber = "F67",
+                Balance = 500,
+                User = new User()
+                {
+                    UserName = "Mary",
+                    LastName = "Pascal",
+                    Age = 23,
+
+                }
+            };
+
+          
+
+            Console.WriteLine($"User1 {user1}\nUser2 {user2}");
+            Console.WriteLine("--------------");
+
+            Bank.Transaction(user1,user2,300);
+            Bank.Transaction(user2, user1, 200);
+            Bank.AddCash(1000,user2);
+            user2.CashOut(2000);    
+            
+
+            Console.WriteLine("--------------");
+
+            Console.WriteLine($"User1 {user1}\nUser2 {user2}");
+
+
+
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"/BankTransfer";
+            DirectoryInfo dir = new DirectoryInfo(path);
+
+            if (!dir.Exists)
+                dir.Create();
+            DirectoryInfo subDir = dir.CreateSubdirectory("UserFolder");
+            FileInfo fileInfo = new FileInfo(subDir.FullName + @"/User.txt");
+
+
+
+            Bank.Writer(fileInfo, Bank.Transaction(user2, user1, 200));
+            Bank.Writer(fileInfo, Bank.Transaction(user1, user2, 200));
+            Bank.Writer(fileInfo, Bank.AddCash(1000, user2));
+            Console.WriteLine("-----------");
+            Bank.Reader(fileInfo.FullName);
+
+
+
+        }
+    }
+}
